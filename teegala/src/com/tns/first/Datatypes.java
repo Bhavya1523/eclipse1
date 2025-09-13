187f22d71973a1b86b2c02258ac351859bbcc3af
vaishnavi <23r95a1209@tkrec.ac.in>
2025-09-13 07:51:13 +0530

    data types

---
+++ teegala/src/com/tns/first/Datatypes.java
@@ -0,0 +1,36 @@
+package com.tns.first;
+
+public class Datatypes {
+	
+	    public static void main(String[] args) {
+	        
+	        // Integer types
+	        byte byteValue = 100;          
+	        short shortValue = 10000;      
+	        int intValue = 100000;         
+	        long longValue = 10000000000L; 
+
+	        // Floating-point types
+	        float floatValue = 10.5f;      
+	        double doubleValue = 20.99;    
+
+	        
+	        char charValue = 'A';          
+
+	        
+	        boolean booleanValue = true;  
+
+	        // Displaying all values
+	        System.out.println("Byte Value: " + byteValue);
+	        System.out.println("Short Value: " + shortValue);
+	        System.out.println("Int Value: " + intValue);
+	        System.out.println("Long Value: " + longValue);
+	        System.out.println("Float Value: " + floatValue);
+	        System.out.println("Double Value: " + doubleValue);
+	        System.out.println("Char Value: " + charValue);
+	        System.out.println("Boolean Value: " + booleanValue);
+	    }
+	
+
+
+}
