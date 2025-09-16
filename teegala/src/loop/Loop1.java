be08220405b0f5eaf68e1e19e42e1b027b446018
vaishnavi <23r95a1209@tkrec.ac.in>
2025-09-16 22:06:06 +0530

    loop

--- teegala/src/loop/Loop1.java
+++ teegala/src/loop/Loop1.java
@@ -10,16 +10,14 @@ public class Loop1 {
 
         boolean isPrime = true;
 
-        if (num <= 1) {
-            isPrime = false;   // 0 and 1 are not prime
-        } else {
+       
             for (int i = 2; i < num; i++) {
                 if (num % i == 0) {
                     isPrime = false;
                     break;
                 }
             }
-        }
+        
 
         if (isPrime) {
             System.out.println(num + " is a prime number");
