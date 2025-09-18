45cd46162d939818d8a2f6e20b70bc3810043ca2
vaishnavi <23r95a1209@tkrec.ac.in>
2025-09-18 23:07:44 +0530

    Abstraction

---
+++ teegala/src/abstraction/AbstractDemo.java
@@ -0,0 +1,14 @@
+package abstraction;
+public class AbstractDemo {
+
+	public static void main(String[] args) {
+		Square sq=new Square(8);
+		sq.calArea();
+		sq.show();
+		Rectangle r=new Rectangle(5,4);
+		r.calArea();
+		r.show();
+
+	}
+
+}
---
+++ teegala/src/abstraction/Square.java
@@ -0,0 +1,12 @@
+package abstraction;
+
+public class Square extends Shape{
+	private int side=2;
+	public Square(int side) {
+		this.side=side;
+	}
+	void calArea() {
+		super.area=side*side;
+	}
+
+}
