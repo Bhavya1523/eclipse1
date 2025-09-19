d1ef611c5429199632720f9f0a662bc5a644f8ed
vaishnavi <23r95a1209@tkrec.ac.in>
2025-09-19 19:34:17 +0530

    wrapper class example1

---
+++ teegala/src/wrapperclass/Autounboxing.java
@@ -0,0 +1,19 @@
+package wrapperclass;
+
+public class Autounboxing {
+
+	public static void main(String[] args) {
+		Integer aobj=100;
+		Character bobj='B';
+		Double cobj=20.2;
+		int a=aobj;
+		char b=bobj;
+		double c=cobj;
+		System.out.println("Integer aobj="+aobj+"-> int a= "+a);
+		System.out.println("Character aobj="+bobj+"-> char b= "+b);
+		System.out.println("Double aobj="+cobj+"-> double c= "+c);
+		
+
+	}
+
+}
