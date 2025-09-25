d8aa7997487cc89e72d954db75286e676968eef0
DEEKSHU PONNALA <DEEKSHU PONNALA@LAPTOP-395TORG1>
2025-09-25 09:44:56 +0530

    wait and join

---
+++ teegalaclg/src/com/tns/thread/JoinExample.java
@@ -0,0 +1,16 @@
+package com.tns.thread;
+
+
+public class JoinExample {
+    public static void main(String[] args) {
+        MyThread t = new MyThread();
+        t.start(); 
+
+        try {
+            t.join(); 
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+        System.out.println("Main Thread resumes after join");
+    }
+}
